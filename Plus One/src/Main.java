cc2e58ccb45bf9506a066312c89e48cff3404275
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2023-02-20 22:35:37 +0300

    Solved problem "Plus One"

---
+++ Plus One/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] digits = {9,8,9};
+        System.out.println(Arrays.toString(new Solution().plusOne(digits)));
+    }
+}
