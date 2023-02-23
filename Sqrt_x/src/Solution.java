ed860d6482b2902d5f3b33eda30aeeba59b3065c
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2023-02-23 20:33:51 +0300

    refactoring solution "Sqrt_x"

--- Sqrt_x/src/Solution.java
+++ Sqrt_x/src/Solution.java
@@ -1,18 +1,35 @@
 class Solution {
     public int mySqrt(int x) {
 
-        int t = 1;
+        /*
+        Approach
+
+        Initial number - x
+        Right search border - right, left - left
+
+        1. Set the left border to 1
+        2. Find the middle of the search area middle = (left+right)/2
+        3. If x / 2 == middle => we have found the root of x
+        4. If middle > x / middle => middle is to the right of the desired number, we shift the right border, otherwise we shift the left border
+
+        Time complexity: 𝑂(log⁡𝑛)
+        */
+
+        int l = 1;
         int r = x;
-        while (t <= r) {
-            int mid = (t + r) / 2;
+        while (l <= r) {
+
+            int mid = (l + r) / 2;
+
             if (x / mid == mid) {
                 return mid;
             } else if (mid > x / mid) {
                 r = mid - 1;
             } else {
-                t = mid + 1;
+                l = mid + 1;
             }
         }
+
         return r;
     }
 }
