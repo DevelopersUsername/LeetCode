3fde526b1413c330d00aace3bfbc8eac16fae50d
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2023-03-09 23:55:50 +0300

    Solved problem  "796. Rotate String"

---
+++ Rotate String/src/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+
+        if(s.equals(goal) == true)
+            return true;
+        else if(s.length() != goal.length())
+            return false;
+
+        for(int i = 0; i < s.length(); i++) {
+
+            s = s.substring(1) + s.substring(0, 1);
+            if(s.equals(goal) == true)
+                return true;
+        }
+
+        return false;
+    }
+}
