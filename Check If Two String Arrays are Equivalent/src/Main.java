f929506fc4ff15b4584c5a678c5f67f3d88390c2
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2023-03-11 11:19:03 +0300

    Solved problem "1662. Check If Two String Arrays are Equivalent"

---
+++ Check If Two String Arrays are Equivalent/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] world1 = {"a", "cb"};
+        String[] world2 = {"ab", "c"};
+        System.out.println(new Solution().arrayStringsAreEqual(world1, world2));
+    }
+}
