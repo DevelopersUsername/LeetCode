80750c83c77a7c87abd216b1a3d1b99043f8f934
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2023-04-12 21:32:02 +0300

    Solved problem "2185. Counting Words With a Given Prefix"

---
+++ Counting Words With a Given Prefix/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().prefixCount(new String[] {"pay", "attention", "practice", "attend"}, "at"));
+    }
+}
