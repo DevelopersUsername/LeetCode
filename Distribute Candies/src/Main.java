6af7c184256fd4bd8c5ffc17356c920e813ee969
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2023-04-17 21:22:52 +0300

    Solved problem "575. Distribute Candies"

---
+++ Distribute Candies/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+            System.out.println(new Solution().distributeCandies(new int[] {6,6,6,6}));
+    }
+}
---
+++ Distribute Candies/src/Solution.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int distributeCandies(int[] candyType) {
+
+        Set<Integer> set = new HashSet<>();
+        for (int i : candyType) {
+            set.add(i);
+        }
+
+        return Math.min(set.size(), candyType.length / 2);
+    }
+}
