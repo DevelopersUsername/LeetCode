10440f5ea3aa730d43258b7761009f2eafb5ceb6
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2023-04-17 21:13:45 +0300

    Solved problem "349. Intersection of Two Arrays"

---
+++ Intersection of Two Arrays/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().intersection(new int[] {1,2,2,1}, new int[] {2,2}));
+    }
+}
---
+++ Intersection of Two Arrays/src/Solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        for (int i : nums1) {
+            set1.add(i);
+        }
+
+        for (int i : nums2) {
+            if (set1.contains(i)) {
+                set2.add(i);
+            }
+        }
+
+        int[] ans = new int[set2.size()];
+        int i = 0;
+        for (int num : set2) {
+            ans[i++] = num;
+        }
+
+        return ans;
+    }
+}
