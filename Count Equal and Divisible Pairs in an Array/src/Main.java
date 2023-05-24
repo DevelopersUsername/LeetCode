10c4c0e3adc2a42f3b6472aeb9c853c059129177
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2023-05-24 22:26:50 +0300

    Solved problem "2176. Count Equal and Divisible Pairs in an Array"

---
+++ Count Equal and Divisible Pairs in an Array/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().countPairs(new int[] {5,5,9,2,5,5,9,2,2,5,5,6,2,2,5,2,5,4,3}, 7));
+    }
+}
