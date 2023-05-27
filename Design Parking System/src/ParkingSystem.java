830ef07c74a327293331c4ac80266ade17baac6f
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2023-05-27 16:19:45 +0300

    Solved problem "1603. Design Parking System"

---
+++ Design Parking System/src/ParkingSystem.java
@@ -0,0 +1,20 @@
+class ParkingSystem {
+
+    private int[] space = new int[3];
+    public ParkingSystem(int big, int medium, int small) {
+        this.space[0] = big;
+        this.space[1] = medium;
+        this.space[2] = small;
+    }
+
+    public boolean addCar(int carType) {
+        return --space[carType -1] >= 0;
+    }
+}
+
+/*
+ *
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
