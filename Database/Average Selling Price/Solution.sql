81282cac27748330b1269714eb4137d0091e9eff
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2023-06-12 23:46:00 +0300

    Solved problem "1251. Average Selling Price"

---
+++ Database/Average Selling Price/Solution.sql
@@ -0,0 +1,5 @@
+SELECT P.product_id, ROUND(SUM(P.price * U.units) / SUM(U.units), 2) AS average_price
+FROM Prices P
+         LEFT JOIN UnitsSold U on P.product_id = U.product_id AND P.start_date <= U.purchase_date AND
+                                  P.end_date >= U.purchase_date
+GROUP BY P.product_id
