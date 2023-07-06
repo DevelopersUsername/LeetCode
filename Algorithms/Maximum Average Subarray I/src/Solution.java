d1e26203292a154e1054df1ad0fada64c01ce7f5
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2023-07-06 21:51:45 +0300

    Solved problem "643. Maximum Average Subarray I"

---
+++ Algorithms/Maximum Average Subarray I/src/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+
+        double ans, sum = 0.0;
+        for (int i = 0; i < k; i++)
+            sum += nums[i];
+
+        ans = sum;
+        for (int i = k; i < nums.length; i++) {
+            sum += nums[i] - nums[i - k];
+            ans = Math.max(ans, sum);
+        }
+
+        return ans / k;
+    }
+}
