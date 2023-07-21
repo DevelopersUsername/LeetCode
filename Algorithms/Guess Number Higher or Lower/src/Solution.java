860832c378b6f7f045c3e2729506c3df767cf92f
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2023-07-21 22:31:44 +0300

    Solved problem "374. Guess Number Higher or Lower"

---
+++ Algorithms/Guess Number Higher or Lower/src/Solution.java
@@ -0,0 +1,25 @@
+/**
+ * Forward declaration of guess API.
+ * // * @param  num   your guess
+ * // * @return 	     -1 if num is higher than the picked number
+ * // *			      1 if num is lower than the picked number
+ * // *               otherwise return 0
+ * // * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+
+        int l = 1, r = n;
+        while (l < r) {
+
+            int m = l + (r - l) / 2, pick = guess(m);
+
+            if (pick == 0) return m;
+            else if (pick < 0) r = m;
+            else l = m + 1;
+        }
+
+        return l;
+    }
+}
