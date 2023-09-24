a5edf779cc03af6ccd868aa8346b0027bba6a08a
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2023-09-24 18:37:31 +0300

    Solve problem "2733. Neither Minimum nor Maximum"

---
+++ Algorithms/Neither Minimum nor Maximum/src/Solution.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public int findNonMinOrMax(int[] nums) {
+
+        if (nums.length < 3) return -1;
+        else Arrays.sort(nums, 0, 3);
+
+        return nums[1];
+    }
+}
