7d3c0c6bb0647e333c534f413096deb172b70ccc
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2023-10-26 22:49:50 +0300

    Solve problem "535. Encode and Decode TinyURL"

---
+++ Algorithms/Encode and Decode TinyURL/src/Codec.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Codec {
+
+    public static List<String> urls = new ArrayList<>();
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        urls.add(longUrl);
+        return String.valueOf(urls.size() - 1);
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        if (urls.isEmpty()) return "";
+        else return urls.get(Integer.parseInt(shortUrl));
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
