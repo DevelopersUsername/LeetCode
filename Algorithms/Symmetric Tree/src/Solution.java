ed89ec2f8c3d1db286a7f1c08207872ba5cf13ff
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2023-11-14 15:07:52 +0300

    Solve problem "101. Symmetric Tree"

---
+++ Algorithms/Symmetric Tree/src/Solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private boolean isSymmetric = true;
+    public boolean isSymmetric(TreeNode root) {
+
+        dfs(root, root);
+
+        return isSymmetric;
+    }
+
+    private void dfs(TreeNode rootLeft, TreeNode rootRight) {
+
+        if (!isSymmetric) return;
+        else if (rootLeft == null && rootRight == null) return;
+        else if (rootLeft == null || rootRight == null) {
+            isSymmetric = false;
+            return;
+        } else if (rootLeft.val != rootRight.val) isSymmetric = false;
+
+        dfs(rootLeft.left, rootRight.right);
+        dfs(rootLeft.right, rootRight.left);
+    }
+}
