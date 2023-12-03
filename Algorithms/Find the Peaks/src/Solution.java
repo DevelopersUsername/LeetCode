5df739ef9fc8cb0eba37eabcc67ccf2a4cf18053
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2023-12-03 19:12:38 +0300

    Solve problem "2951. Find the Peaks"

---
+++ Algorithms/Find the Peaks/src/Solution.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> findPeaks(int[] mountain) {
+
+        final List<Integer> ans = new LinkedList<>();
+        if (mountain.length < 3) return ans;
+
+        for (int i = 1; i < mountain.length - 1; i++)
+            if (mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1]) ans.add(i);
+
+        return ans;
+    }
+}
