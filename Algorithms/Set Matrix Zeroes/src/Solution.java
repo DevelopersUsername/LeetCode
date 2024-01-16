fca04554052ac13edd239d01824a508e76187777
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2024-01-16 20:12:49 +0300

    Solve problem "73. Set Matrix Zeroes"

---
+++ Algorithms/Set Matrix Zeroes/src/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+
+        int m = matrix.length, n = matrix[0].length;
+        boolean[] row = new boolean[m], column = new boolean[n];
+
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    column[j] = true;
+                }
+
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (row[i] || column[j]) {
+                    matrix[i][j] = 0;
+                }
+    }
+}
