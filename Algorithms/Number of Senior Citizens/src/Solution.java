5ddd110069f98f810792a3a3c0b6eec114122837
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2024-03-28 22:53:41 +0300

    Solve problem "2678. Number of Senior Citizens"

---
+++ Algorithms/Number of Senior Citizens/src/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int countSeniors(String[] details) {
+
+        int ans = 0;
+        for (String detail : details)
+            if (((detail.charAt(11) - '0') * 10 + detail.charAt(12) - '0') > 60) ans++;
+
+        return ans;
+    }
+}
