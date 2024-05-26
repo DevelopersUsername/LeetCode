e1350f2d86e3f71140cf64b6552164f64f116625
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2024-05-26 22:12:53 +0300

    Solve problem "950. Reveal Cards In Increasing Order"

---
+++ Algorithms/Reveal Cards In Increasing Order/src/Solution.java
@@ -0,0 +1,24 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+
+        Arrays.sort(deck);
+        Deque<Integer> dq = new ArrayDeque<>();
+        int n = deck.length;
+
+        dq.addFirst(deck[n - 1]);
+        for (int i = n - 2; i >= 0; i--) {
+            dq.addFirst(dq.getLast());
+            dq.pollLast();
+            dq.addFirst(deck[i]);
+        }
+
+        for (int i = 0; i < n; ++i)
+            deck[i] = dq.pollFirst();
+
+        return deck;
+    }
+}
