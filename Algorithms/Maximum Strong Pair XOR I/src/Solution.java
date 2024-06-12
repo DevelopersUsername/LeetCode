2fb3c6ea5871ec5f98a7312ac2b2318d68d58c90
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2024-06-12 16:58:18 +0300

    Solve problem "2932. Maximum Strong Pair XOR I"

---
+++ Algorithms/Maximum Strong Pair XOR I/src/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maximumStrongPairXor(int[] nums) {
+
+        int ans = 0;
+        for (int i : nums)
+            for (int j : nums)
+                if (Math.abs(i - j) <= Math.min(i, j))
+                    ans = Math.max(ans, i ^ j);
+
+        return ans;
+    }
+}
