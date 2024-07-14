9476993a709bbdc490deca9495e7ee622157dabc
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2024-07-14 23:14:17 +0300

    Solve problem "1833. Maximum Ice Cream Bars"

---
+++ Algorithms/Maximum Ice Cream Bars/src/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+
+        Arrays.sort(costs);
+        for (int i = 0; i < costs.length; i++)
+            if (costs[i] <= coins) coins -= costs[i];
+            else return i;
+
+        return costs.length;
+    }
+}
