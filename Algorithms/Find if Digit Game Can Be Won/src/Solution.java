53879aa004a37f02a5f08d8407532565874c6d57
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2024-08-08 22:53:19 +0300

    Solve problem "3232. Find if Digit Game Can Be Won"

---
+++ Algorithms/Find if Digit Game Can Be Won/src/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+
+        int s = 0, d = 0;
+        for (int num : nums)
+            if (num < 10) s += num;
+            else d += num;
+
+        return s != d;
+    }
+}
