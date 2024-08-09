f8f9746ca96c4996927fefaa75e159f92304863c
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2024-08-09 18:30:23 +0300

    Solve problem "3184. Count Pairs That Form a Complete Day I"

---
+++ Algorithms/Count Pairs That Form a Complete Day I/src/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countCompleteDayPairs(int[] hours) {
+
+        int ans = 0;
+        int[] count = new int[24];
+
+        for (int hour : hours) {
+            ans += count[(24 - hour % 24) % 24];
+            count[hour % 24]++;
+        }
+
+        return ans;
+    }
+}
