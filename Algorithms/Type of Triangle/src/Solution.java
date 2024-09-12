e44dd27ea72f7eec2f3cb2d000823e4bdc27ded8
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2024-09-12 20:36:51 +0300

    Solve problem "3024. Type of Triangle"

---
+++ Algorithms/Type of Triangle/src/Solution.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public String triangleType(int[] nums) {
+
+        Arrays.sort(nums);
+        if (nums[0] + nums[1] <= nums[2]) return "none";
+        else if (nums[0] == nums[1] && nums[1] == nums[2]) return "equilateral";
+        else if (nums[0] == nums[1] || nums[1] == nums[2]) return "isosceles";
+        else return "scalene";
+    }
+}
