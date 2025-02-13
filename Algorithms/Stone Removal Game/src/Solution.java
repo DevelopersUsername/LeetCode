76fffcfa07dbc5e71a45ee01dd71b000053058a4
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2025-02-13 20:33:14 +0300

    Solve problem '3360. Stone Removal Game'

---
+++ Algorithms/Stone Removal Game/src/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canAliceWin(int n) {
+
+        for (int stones = 10; stones >= 0; stones--) {
+            if (stones > n)
+                return stones % 2 == 1;
+            n -= stones;
+        }
+
+        return false;
+    }
+}
