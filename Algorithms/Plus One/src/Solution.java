b29b66119c2106ab93101c96759510d2866e9cf7
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2025-03-22 21:47:42 +0300

    Solve problem '66. Plus One'

--- Algorithms/Plus One/src/Solution.java
+++ Algorithms/Plus One/src/Solution.java
@@ -1,20 +1,17 @@
-import java.util.Arrays;
-
-public class Solution {
+class Solution {
     public int[] plusOne(int[] digits) {
 
-        for(int i = digits.length - 1; i>= 0; i--){
-
-            if(digits[i] < 9){
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
                 digits[i]++;
                 return digits;
             }
+
             digits[i] = 0;
         }
 
-        int[] result = new int[digits.length + 1];
-        result[0] = 1;
-
-        return result;
+        int[] ans = new int[digits.length + 1];
+        ans[0] = 1;
+        return ans;
     }
 }
