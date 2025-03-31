4325442d42e78a5abacc2801694b3439907696d0
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2025-03-31 20:47:03 +0300

    Solve problem '3483. Unique 3-Digit Even Numbers'

---
+++ Algorithms/Unique 3-Digit Even Numbers/src/Solution.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int totalNumbers(int[] digits) {
+
+        Set<Integer> unique = new HashSet<>();
+        int n = digits.length;
+
+        for (int i = 0; i < n; ++i) {
+            if (digits[i] % 2 == 1) continue;
+
+            for (int j = 0; j < n; ++j) {
+
+                if (i == j) continue;
+                for (int k = 0; k < n; ++k) {
+                    if (digits[k] == 0 || k == i || k == j) continue;
+                    unique.add(digits[k] * 100 + digits[j] * 10 + digits[i]);
+                }
+            }
+        }
+        return unique.size();
+    }
+}
