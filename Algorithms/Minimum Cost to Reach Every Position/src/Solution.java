eb5a3f0d6081d9939b568a78e6ac74796c215524
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2025-04-09 19:42:19 +0300

    Solve problem '3502. Minimum Cost to Reach Every Position'

---
+++ Algorithms/Minimum Cost to Reach Every Position/src/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] minCosts(int[] cost) {
+
+        for (int i = 1; i < cost.length; i++)
+            cost[i] = Math.min(cost[i-1], cost[i]);
+
+        return cost;
+    }
+}
