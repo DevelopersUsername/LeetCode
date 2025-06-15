a25bf3ccc29c7994a6a9bf3b21aad2cd45f3596e
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2025-06-15 09:31:50 +0300

    Solve problem '53. Maximum Subarray'

---
+++ Algorithms/Maximum Subarray/src/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int ans = Integer.MIN_VALUE, sum = 0;
+
+        for (int num : nums) {
+            sum = Math.max(sum + num, num);
+            ans = Math.max(ans, sum);
+        }
+
+        return ans;
+    }
+}
