2537f6d341f84fbb4eb397a0935ba35f8b2c7cfc
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2025-07-02 12:34:34 +0300

    Solve a problem '148. Sort List'

---
+++ Algorithms/Sort List/src/Solution.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+
+        if (head == null || head.next == null) return head;
+
+        ListNode prev = head, slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        prev.next = null;
+
+        return merge(sortList(head), sortList(slow));
+    }
+
+    private static ListNode merge(ListNode left, ListNode right) {
+
+        if (left == null) return right;
+        else if (right == null) return left;
+        else if (left.val < right.val) {
+            left.next = merge(left.next, right);
+            return left;
+        } else {
+            right.next = merge(left, right.next);
+            return right;
+        }
+    }
+}
