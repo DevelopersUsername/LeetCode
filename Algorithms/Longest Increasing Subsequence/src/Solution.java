da12f15e617fc71e8be7276bd1be66a7269b9f67
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2025-08-07 19:17:59 +0300

    Solve a problem '300. Longest Increasing Subsequence'

---
+++ Algorithms/Longest Increasing Subsequence/src/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+
+        int ans = 1, n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < n; i++)
+            for (int j = 0; j < i; j++)
+                if (nums[i] > nums[j] && dp[i] < dp[j] + 1)
+                    dp[i] = dp[j] + 1;
+
+        for (int i = 0; i < n; i++)
+            ans = Math.max(ans, dp[i]);
+
+        return ans;
+    }
+}
