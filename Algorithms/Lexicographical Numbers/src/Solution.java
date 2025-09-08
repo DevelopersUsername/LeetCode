60762b16aa1b71d8e01838d1b0bdda8bfa434ed4
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2025-09-08 23:41:18 +0300

    Solve a problem '386. Lexicographical Numbers'

---
+++ Algorithms/Lexicographical Numbers/src/Solution.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+
+        List<Integer> ans = new ArrayList<>(n);
+
+        int num = 1, count = 0;
+        while (count < n) {
+            ans.add(num);
+            count++;
+            if (num * 10 <= n) num *= 10;
+            else {
+                while (num % 10 == 9 || num == n) num /= 10;
+                num++;
+            }
+        }
+
+        return ans;
+    }
+}
