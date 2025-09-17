226800fa0fddecc7af61011d019f1e21649e3691
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2025-09-17 23:57:18 +0300

    Solve a problem '213. House Robber II'

---
+++ Algorithms/House Robber II/src/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 0) return 0;
+        else if (nums.length == 1) return nums[0];
+        else return Math.max(rob(nums, 0, nums.length - 2), rob(nums, 1, nums.length - 1));
+    }
+
+    private static int rob(int[] nums, int l, int r) {
+
+        int prev1 = 0, prev2 = 0;
+        for (int i = l; i <= r; i++) {
+            int dp = Math.max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = dp;
+        }
+
+        return prev1;
+    }
+}
