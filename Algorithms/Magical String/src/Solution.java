fe83a51dfc2614a5a10e29de14a10d37914843bf
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2025-09-25 20:31:40 +0300

    Solve a problem '481. Magical String'

---
+++ Algorithms/Magical String/src/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int magicalString(int n) {
+
+        StringBuilder sb = new StringBuilder(" 122");
+
+        for (int i = 3; i <= n; i++)
+            if (i % 2 == 1) sb.append(String.valueOf('1').repeat(sb.charAt(i) - '0'));
+            else sb.append(String.valueOf('2').repeat(sb.charAt(i) - '0'));
+
+        int ans = 0;
+        for (int i = 0; i <= n; i++)
+            if (sb.charAt(i) == '1') ans++;
+
+        return ans;
+    }
+}
