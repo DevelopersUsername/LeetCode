9030545b76be01a400f90c4181215c055d4789a7
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2025-10-21 22:12:44 +0300

    Solve a problem '1806. Minimum Number of Operations to Reinitialize a Permutation'

---
+++ Algorithms/Minimum Number of Operations to Reinitialize a Permutation/src/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reinitializePermutation(int n) {
+
+        int ans = 0, i = 1;
+
+        do {
+            if (i < n / 2) i = i * 2;
+            else i = (i - n / 2) * 2 + 1;
+            ans++;
+        } while (i != 1);
+
+        return ans;
+    }
+}
