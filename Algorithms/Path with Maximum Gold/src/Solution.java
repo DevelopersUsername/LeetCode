ea6b61d747509907addbd9eb127e4691dcdceb18
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2025-11-18 19:34:38 +0300

    Solve a problem '1219. Path with Maximum Gold'

---
+++ Algorithms/Path with Maximum Gold/src/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int getMaximumGold(int[][] grid) {
+
+        int ans = 0;
+        for (int i = 0; i < grid.length; i++)
+            for (int j = 0; j < grid[0].length; j++)
+                ans = Math.max(ans, dfs(grid, i, j));
+
+        return ans;
+    }
+
+    private static int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i == grid.length || j == grid[0].length) return 0;
+        else if (grid[i][j] == 0) return 0;
+
+        int gold = grid[i][j];
+        grid[i][j] = 0;
+        int maxGold = Math.max(
+                Math.max(dfs(grid, i + 1, j), dfs(grid, i - 1, j)),
+                Math.max(dfs(grid, i, j + 1), dfs(grid, i, j - 1))
+        );
+        grid[i][j] = gold;
+
+        return gold + maxGold;
+    }
+}
