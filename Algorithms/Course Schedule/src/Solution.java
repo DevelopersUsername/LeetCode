a6a6551404c1d1471c3ba6198979584360673d5e
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2025-12-28 23:23:19 +0300

    Refactor a problem '207. Course Schedule'

--- Algorithms/Course Schedule/src/Solution.java
+++ Algorithms/Course Schedule/src/Solution.java
@@ -1,11 +1,15 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 class Solution {
     private final Map<Integer, List<Integer>> preMap = new HashMap<>();
-    private final Set<Integer> visiting = new HashSet<>();
+    private boolean[] visited;
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
 
+        visited = new boolean[numCourses];
         for (int i = 0; i < numCourses; i++) preMap.put(i, new ArrayList<>());
         for (int[] prereq : prerequisites) preMap.get(prereq[0]).add(prereq[1]);
 
@@ -17,14 +21,14 @@ class Solution {
 
     private boolean isPossible(int course) {
 
-        if (visiting.contains(course)) return false;
+        if (visited[course]) return false;
         else if (!preMap.containsKey(course)) return true;
 
-        visiting.add(course);
+        visited[course] = true;
         for (int pre : preMap.get(course))
             if (!isPossible(pre)) return false;
 
-        visiting.remove(course);
+        visited[course] = false;
         preMap.remove(course);
 
         return true;
