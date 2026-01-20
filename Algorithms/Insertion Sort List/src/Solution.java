b9777e93891aee82ecaf87df903c5f75c3a4a3af
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2026-01-20 19:51:10 +0300

    Solve a problem '147. Insertion Sort List'

---
+++ Algorithms/Insertion Sort List/src/Solution.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+
+        List<Integer> arr = new ArrayList<>();
+        ListNode cur = head;
+
+        while (cur != null) {
+            arr.add(cur.val);
+            cur = cur.next;
+        }
+
+        Collections.sort(arr);
+
+        cur = head;
+        for (int val : arr) {
+            cur.val = val;
+            cur = cur.next;
+        }
+
+        return head;
+    }
+}
