b7ea20b999f636dcee8e495908b7e0d2159cf26f
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2026-02-11 18:18:18 +0300

    Solve a problem '1545. Find Kth Bit in Nth Binary String'

---
+++ Algorithms/Find Kth Bit in Nth Binary String/src/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public char findKthBit(int n, int k) {
+
+        if (n == 1) return '0';
+
+        int midIndex = (int) Math.pow(2, n - 1);
+        if (k == midIndex) return '1';
+        else if (k < midIndex) return findKthBit(n - 1, k);
+        else return findKthBit(n - 1, midIndex * 2 - k) == '0' ? '1' : '0';
+    }
+}
