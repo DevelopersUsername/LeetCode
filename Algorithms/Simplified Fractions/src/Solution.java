d8001cc5c620bb78bde600952c66ede901d5442c
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2026-02-17 21:20:30 +0300

    Solve a problem '1447. Simplified Fractions'

---
+++ Algorithms/Simplified Fractions/src/Solution.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> simplifiedFractions(int n) {
+
+        List<String> ans = new ArrayList<>();
+        for (int denominator = 2; denominator <= n; denominator++)
+            for (int numerator = 1; numerator < denominator; numerator++)
+                if (gcd(denominator, numerator) == 1)
+                    ans.add(numerator + "/" + denominator);
+        return ans;
+    }
+
+    private static int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+}
