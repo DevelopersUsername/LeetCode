41d57edde33f8d819eb212b7dc341507a437ad10
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2026-02-27 21:49:16 +0300

    Solve a problem '1400. Construct K Palindrome Strings'

---
+++ Algorithms/Construct K Palindrome Strings/src/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canConstruct(String s, int k) {
+
+        if (k > s.length()) return false;
+
+        int[] count = new int[26];
+        for (char c : s.toCharArray())
+            count[c - 'a']++;
+
+        int ans = 0;
+        for (int c : count)
+            if (c % 2 != 0) ans++;
+
+        return ans <= k;
+    }
+}
