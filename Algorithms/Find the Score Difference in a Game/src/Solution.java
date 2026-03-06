6e0eb2d30b5e9d6f12714ee076bfb86b6f35c883
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2026-03-06 22:21:36 +0300

    Solve a problem '3847. Find the Score Difference in a Game'

---
+++ Algorithms/Find the Score Difference in a Game/src/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int scoreDifference(int[] nums) {
+
+        int ans = 0, k = 1;
+        for (int i = 0; i < nums.length; i++) {
+            int x = nums[i];
+            if ((x & 1) == 1) k = -k;
+            if (i % 6 == 5) k = -k;
+            ans += k * x;
+        }
+
+        return ans;
+    }
+}
